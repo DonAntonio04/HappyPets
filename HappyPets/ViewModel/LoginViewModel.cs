f8600780e642f8d7b1be23957ef6939dcf118183
DonAntonio04 <berrellezantonio04@gmail.com>
2024-04-19 13:21:34 -0700

    avance2

--- HappyPets/ViewModel/LoginViewModel.cs
+++ HappyPets/ViewModel/LoginViewModel.cs
@@ -1,10 +1,16 @@
-﻿using HappyPets.Views.LoginFolder;
+﻿using Firebase.Auth;
+using Firebase.Database;
+using HappyPets.Models;
+using HappyPets.Views.LoginFolder;
 using HappyPets.Views.MainMenu;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace HappyPets.ViewModel
@@ -37,18 +43,62 @@ namespace HappyPets.ViewModel
         #endregion
 
         #region METHODS
-        public async Task GoToMainMenu()
+        //public async Task GoToMainMenu()
+        //{
+        //    if (Email == string.Empty || Password == string.Empty)
+        //    {
+        //        await DisplayAlert("Hace falta algo", "Alguno de los campos esta vácio, verifica tú información!", "Ok");
+        //    }
+
+        //    else
+        //    {
+        //        await Navigation.PushAsync(new Main());
+        //    }
+
+        //}
+
+
+
+        public async Task VerificationAndGoToMain()
         {
+            FirebaseClient firebaseClient = new FirebaseClient("https://happydogsdb-default-rtdb.firebaseio.com/");
+
+            var user = (await firebaseClient
+              .Child("Users")
+              .OnceAsync<UsersModel>()).FirstOrDefault(a => a.Object.Email == Email);
+
+            if (user != null)
+            {
+               //await DisplayAlert("Exito","Correo Encontrado","Ok");
+
+                if (user.Object.Password == Password)
+                {
+                  await DisplayAlert("Exito","Contraseña autenticada exitosamente", "ok");
+                  await Navigation.PushAsync(new Main());
+                }
+                else
+                {
+                   await DisplayAlert("Error","Contraseña incorrecta","Ok");
+                }
+            }
+            else
+            {
+               await DisplayAlert("Error","Usuario no encontrado","Ok");
+            }
+
             if (Email == string.Empty || Password == string.Empty)
             {
                 await DisplayAlert("Hace falta algo", "Alguno de los campos esta vácio, verifica tú información!", "Ok");
             }
 
-            {
-                await Navigation.PushAsync(new Main());
-            }
+          
+            
+               
+            
 
         }
+
+
         public async Task GoToRegister()
         {
             await Navigation.PushAsync(new Register());
@@ -59,7 +109,7 @@ namespace HappyPets.ViewModel
         #region COMMANDS
 
 
-        public ICommand GoToMainMenuCommand => new Command(async () => await GoToMainMenu());
+        public ICommand VerificationAndGoToMainMenuCommand => new Command(async () => await VerificationAndGoToMain());
         public ICommand GoToRegisterCommand => new Command(async () => await GoToRegister());
         #endregion 
     }
